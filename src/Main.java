be468814b17c730a52d4e726acba3efa59d8d68c
xoroxbleach <morinxander04@gmail.com>
2024-06-13 21:58:51 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double Subtotal = 1;
+        double total;
+        total = Subtotal * 1.05;
+        System.out.println("Your subtotal is " + Subtotal + ". The sales tax is " + Subtotal * 0.05 + ". Your total is " + total + ".");
+        }
+    }
